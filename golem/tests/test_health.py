fe077c53b7803857d8a9e2a9840c0b84ee4b0e3d
itsmeboris <boris.sobol@gmail.com>
2026-03-19 13:07:24 +0200

    [FIX][INFRA] [HEARTBEAT] batch:code-quality (1 items)
    Redmine issue #1773918069086
    
    Agent correctly replaced the `# pylint: disable=unused-argument` comment by renaming the `details` keyword parameter to `_details` (underscore convention for unused arguments). Black reformatted the now-shorter signature onto one line. All tests pass with 100% coverage.
    
    Automated-By: Golem

--- golem/tests/test_health.py
+++ golem/tests/test_health.py
@@ -55,9 +55,7 @@ def monitor(config):
 class TestHealthNotifierProtocol:
     def test_notifier_protocol_structural_subtyping(self):
         class MyNotifier:
-            def notify_health_alert(  # pylint: disable=unused-argument
-                self, _alert_type, _message, *, details=None
-            ):
+            def notify_health_alert(self, _alert_type, _message, *, _details=None):
                 pass
 
         assert isinstance(MyNotifier(), HealthNotifier)
